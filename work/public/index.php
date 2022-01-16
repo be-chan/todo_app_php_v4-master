d99eb9f5e07d636fb872170f4a71deb1fad53983
be-chan <70454487+be-chan@users.noreply.github.com>
2022-01-16 23:06:21 +0900

    Todo app
    # class part

--- work/public/index.php
+++ work/public/index.php
@@ -1,66 +1,16 @@
 <?php
 
+require_once(__DIR__ . '/../app/config.php');
 
+use Myapp\Database;
+use Myapp\Todo;
+use Myapp\Utils;
 
-define('DSN', 'mysql:host=db;dbname=myapp;charset=utf8mb4');
-define('DB_USER', 'myappuser');
-define('DB_PASS', 'myapppass');
-// define('SITE_URL', 'http://localhost:8562');
-define('SITE_URL', 'http://' . $SERVER['HTTP_HOST']);
+$pdo = Database::getInstance();
 
-try {
-  $pdo = new PDO(
-    DSN,
-    DB_USER,
-    DB_PASS,
-    [
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-      PDO::ATTR_EMULATE_PREPARES => false,
-    ]
-  );
-} catch (PDOException $e) {
-  echo $e->getMessage();
-  exit;
-}
-
-function addTodo($pdo)
-{
-  $title = trim(filter_input(INPUT_POST, 'title'));
-  if($title === '') {
-    return;
-  }
-
-  $stmt = $pdo->prepare("INSERT INTO todos (title) VALUES (:title)");
-  $stmt->bindValue('title', $title, PDO::PARAM_STR);
-  $stmt->execute();
-}
-
-function getTodos($pdo)
-{
-  $stmt = $pdo->query("SELECT * FROM todos ORDER BY id DESC");
-  $todos = $stmt->fetchall();
-  return $todos;
-}
-
-function h($str)
-{
-  return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
-}
-
-function createToken() 
-{
-  
-}
-
-if($_SERVER['REQUEST_METHOD'] === 'POST') {
-  addTodo($pdo);
-
-  header('Location: ' . SITE_URL);
-  exit;
-}
-
-$todos = getTodos($pdo);
+$todo = new Todo($pdo);
+$todo->processPost();
+$todos = $todo->getAll();
 ?>
 
 <!DOCTYPE html>
@@ -71,21 +21,41 @@ $todos = getTodos($pdo);
   <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-  <h1>Todos</h1>
+  <main>
+    <header>
+      <h1>Todos</h1>
+      <form action="?action=purge" method="post">
+        <span class="purge">Purge</span>
+        <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']);?>">
+      </form>
+    </header> 
+    <form action="?action=add" method="post">
+      <input type="text" name="title" placeholder="Type new todo.">
+      <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']);?>">
+    </form>
 
-  <form action="" method="post">
-    <input type="text" name="title" placeholder="Type new todo.">
-  </form>
+    <ul>
+      <?php foreach ($todos as $todo): ?>
+        <li>
+          <form action="?action=toggle" method="post">
+            <input type="checkbox" <?= $todo->is_done ? 'checked' : '';?>>
+            <input type="hidden" name="id" value="<?= Utils::h($todo->id);?>">
+            <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']);?>">
+          </form>
+          <span class="<?= $todo->is_done ? 'done' : ''; ?>">
+            <?= Utils::h($todo->title); ?>
+          </span>
 
-  <ul>
-    <?php foreach ($todos as $todo): ?>
-      <li>
-        <input type="checkbox" <?= $todo->is_done ? 'checked' : '';?>>
-        <span class="<?= $todo->is_done ? 'done' : ''; ?>">
-          <?= h($todo->title); ?>
-        </span>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+          <form action="?action=delete" method="post" class="delete-form">
+            <span class="delete">x</span>
+            <input type="hidden" name="id" value="<?= Utils::h($todo->id);?>">
+            <input type="hidden" name="token" value="<?= Utils::h($_SESSION['token']);?>">
+          </form>
+          
+        </li>
+      <?php endforeach; ?>
+    </ul>
+  </main>
+  <script src="js/main.js"></script>
 </body>
 </html>
